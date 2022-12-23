a62aee0e26898ef2a5578400e19b9a5fdbac6385
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-23 14:50:18 +0900

    [Fix]現在、サウンドマネージャーの修正がまだ出来ていないため一部コメントアウトしてエラーを回避します。

--- Assets/Works/Sawada/Scripts/SceneInitializer.cs
+++ Assets/Works/Sawada/Scripts/SceneInitializer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class SceneInitializer : MonoBehaviour
 {
     [SerializeField, Tooltip("ƒTƒEƒ“ƒh‚Ìî•ñ‚ğ‚ÂScriptableObject")]
-    SceneManager _soundScriptableObj = null;
+    //SceneManager _soundScriptableObj = null;
 
     private void Awake()
     {
-        if (GameManager.InstanceScene == null)
-        {
-            GameManager.InstanceScene = _soundScriptableObj;
-        }
+        //if (GameManager.InstanceScene == null)
+        //{
+        //    GameManager.InstanceScene = _soundScriptableObj;
+        //}
     }
 }
